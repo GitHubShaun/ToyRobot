d02bdf0267db58a747018544a8158beba5e23d08
Shaun <shaun.c.au@gmail.com>
2019-06-27 18:55:53 +0800

    Additional functionality.  - Added direction enum (NORTH, SOUTH, EAST WEST)  - Second constructor taking in a string, ie. "0,0,NORTH"  - boolean function to check for valid string

--- src/toyrobot/ToyRobot.java
+++ src/toyrobot/ToyRobot.java
@@ -2,67 +2,84 @@ package toyrobot;
 
 public class ToyRobot {
 
+    enum Direction {
+        NORTH, SOUTH, EAST, WEST
+    }
+
     private int X;
     private int Y;
-    private String DIRECTION;
+    private Direction DIRECTION;
 
-    public ToyRobot(int X, int Y, String DIRECTION){
+    public ToyRobot(int X, int Y, Direction DIRECTION){
         this.X = X;
         this.Y = Y;
         this.DIRECTION = DIRECTION;
     }
 
+    public ToyRobot(String position){
+        if(validPlaceCommand(position)){
+            this.X = Integer.parseInt(position.substring(0,position.indexOf(",")));
+            this.Y = Integer.parseInt(position.substring(position.indexOf(",") + 1, position.lastIndexOf(",")));
+            this.DIRECTION = toDirection(position.toUpperCase().substring(position.lastIndexOf(",")+1));
+        }
+    }
+
+    public int getX(){ return this.X; }
+
+    public int getY(){ return this.Y; }
+
+    public Direction getDirection(){ return this.DIRECTION; }
+
     public void move(){
-        System.out.println("MOVE COMMAND.");
+        //System.out.println("MOVE COMMAND.");
         switch(this.DIRECTION){
-            case "NORTH":
+            case NORTH:
                 if (this.Y + 1 <= 4 && this.Y + 1 >= 0) { this.Y = this.Y + 1; }
                 break;
-            case "SOUTH" :
+            case SOUTH:
                 if (this.Y - 1 <= 4 && this.Y - 1 >= 0) { this.Y = this.Y - 1; }
                 break;
-            case "EAST" :
+            case EAST:
                 if (this.X + 1 <= 4 && this.X + 1 >= 0) { this.X = this.X + 1; }
                 break;
-            case "WEST" :
+            case WEST:
                 if (this.X - 1 <= 4 && this.X - 1 >= 0) { this.X = this.X - 1; }
                 break;
         }
     }
 
     public void left(){
-        System.out.println("LEFT COMMAND.");
+        //System.out.println("LEFT COMMAND.");
         switch(this.DIRECTION){
-            case "NORTH":
-                this.DIRECTION = "WEST";
+            case NORTH:
+                this.DIRECTION = Direction.WEST;
                 break;
-            case "SOUTH" :
-                this.DIRECTION = "EAST";
+            case SOUTH:
+                this.DIRECTION = Direction.EAST;
                 break;
-            case "EAST" :
-                this.DIRECTION = "NORTH";
+            case EAST:
+                this.DIRECTION = Direction.NORTH;
                 break;
-            case "WEST" :
-                this.DIRECTION = "SOUTH";
+            case WEST:
+                this.DIRECTION = Direction.SOUTH;
                 break;
         }
-
     }
 
     public void right(){
-        System.out.println("RIGHT COMMAND.");
+        //System.out.println("RIGHT COMMAND.");
         switch(this.DIRECTION){
-            case "NORTH":
-                this.DIRECTION = "EAST";
+            case NORTH:
+                this.DIRECTION = Direction.EAST;
                 break;
-            case "SOUTH" :
-                this.DIRECTION = "WEST";
+            case SOUTH:
+                this.DIRECTION = Direction.WEST;
                 break;
-            case "EAST" :
-                this.DIRECTION = "SOUTH";
+            case EAST:
+                this.DIRECTION = Direction.SOUTH;
                 break;
-            case "WEST" :
-                this.DIRECTION = "NORTH";
+            case WEST:
+                this.DIRECTION = Direction.NORTH;
                 break;
         }
     }
@@ -70,5 +87,33 @@ public class ToyRobot {
     public void report(){
         //System.out.println("REPORT COMMAND.");
         System.out.println("Output: " + this.X + "," + this.Y + "," + this.DIRECTION);
+    }
+
+    private boolean validPlaceCommand(String str){
+        return str.toUpperCase().replaceAll("\\s","").matches("([0-4]),([0-4]),(NORTH|SOUTH|EAST|WEST)");
+    }
+
+    private Direction toDirection(String string){
+        assert string != null : "Invalid direction facing, options: [NORTH,SOUTH,EAST,WEST].";
+        Direction tmp = null;
+        try {
+            switch (string) {
+                case "NORTH":
+                    tmp = Direction.NORTH;
+                    break;
+                case "SOUTH":
+                    tmp = Direction.SOUTH;
+                    break;
+                case "EAST":
+                    tmp = Direction.EAST;
+                    break;
+                case "WEST":
+                    tmp = Direction.WEST;
+                    break;
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return tmp;
     }
 }
--- src/toyrobot/ToyRobotSim.java
+++ src/toyrobot/ToyRobotSim.java
@@ -2,19 +2,29 @@ package toyrobot;
 
 public class ToyRobotSim {
 
-    public static void inputHandler(){
+    //test.txt - open file, scan lines
+    //PLACE 0,0,NORTH - input args one by one
+    //(empty) - interactive sim..., reply with dialogue("Enter PLACE X,Y,DIRECTION")
+    public static void inputHandler(String[] args){
+        if (args.length > 0){
 
+        }else{
+            doStuff();
+        }
     }
 
-
-
-    public static void main(String[] args){
-        ToyRobot robot = new ToyRobot(1,2,"EAST");
+    public static void doStuff(){
+        //ToyRobot robot = new ToyRobot(1,2, ToyRobot.Direction.valueOf("WEST"));
+        ToyRobot robot = new ToyRobot("1,2,east");
         robot.move();
         robot.move();
         robot.left();
         robot.move();
         robot.report();
+    }
+
+    public static void main(String[] args){
+        inputHandler(args);
     }
 
 }
